64f904aef5db05dbc74a96573d0c7e2be4fbcaf4
pashutetto <71449006+pashutetto@users.noreply.github.com>
2022-10-01 13:21:00 +0300

    Add files via upload

--- task1.py
+++ task1.py
@@ -1,16 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import math
-import numpy
-import matplotlib.pyplot as mpp
+import math  # подключение библиотеки 
+import numpy  # подключение библиотеки
+import matplotlib.pyplot as mpp  # подключение библиотеки, сокращение названия для вызова библиотеки 
 
 # Эта программа рисует график функции, заданной выражением ниже
 
-if __name__=='__main__':
-    arguments = numpy.arange(0, 200, 0.1)
-    mpp.plot(
-        arguments,
-        [math.sin(a) * math.sin(a/20.0) for a in arguments]
+if __name__=='__main__':  # проверяет как запущен скрипт
+    arguments = numpy.arange(0, 200, 0.1)  # массив аргументов функции от 0 до 200 с шагом 0.1
+    mpp.plot(  # фунцкция, которая "вводит" точки графика
+        arguments,  # координаты точек по оси абсцисс
+        [math.sin(a) * math.sin(a/20.0) for a in arguments]  # координаты точек по оси ординат
     )
-    mpp.show()
+    mpp.show()  # вывод графика на экран
+
+
