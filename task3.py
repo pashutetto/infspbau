83d64e6b2a58051f3f55d9ed343b7782665f4189
pashutetto <71449006+pashutetto@users.noreply.github.com>
2022-10-26 20:46:53 +0300

    Add files via upload

--- task3.py
+++ task3.py
@@ -2,15 +2,6 @@ import turtle as t
 
 def draw_fractal(sz):
     if sz>=30:
-##        t.right(60)
-##        t.forward(sz/2.0)
-##        draw_fractal(sz/2.0)
-##        t.forward(sz/2.0)
-##        t.right(120)
-##        t.forward(sz)
-##        t.right(120)
-##        t.forward(sz)
-##        t.right(60)
         t.right(60)
         t.forward(sz/2.0)
         draw_fractal(sz/2.0)
@@ -23,6 +14,7 @@ def draw_fractal(sz):
         
 
 scale=900
+
 t.speed(0)
 t.tracer(0, 0)
 t.pensize(1)
@@ -30,6 +22,7 @@ t.penup()
 t.color('purple')
 t.goto(0,300)
 t.pendown()
+
 draw_fractal(scale)
 t.update()
 t.done()
