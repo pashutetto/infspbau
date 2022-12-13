b14cdd48c1f574aeb59a220bef383a7e38b8cc60
pashutetto <71449006+pashutetto@users.noreply.github.com>
2022-12-13 20:00:42 +0300

    Add files via upload

--- task11.py
+++ task11.py
@@ -4,16 +4,14 @@ class Fib:
 
     class _Fib_iter:
         def __init__(self):
-            self.i = 0
-            self.fibs=[]
-            self.fibs.insert(0,1) 
-            self.fibs.insert(1,1)
-
+            self.a1=1 
+            self.a2=1
+            
         def __next__(self):
-                j = self.i
-                self.i += 1
-                self.fibs.insert(j+2, self.fibs[j+1]+self.fibs[j])
-                return self.fibs[j]
+                k = self.a1
+                self.a2=self.a1+self.a2
+                self.a1=self.a2-self.a1
+                return k
 
     def __iter__(self):
         return Fib._Fib_iter()
