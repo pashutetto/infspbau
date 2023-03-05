f35610f21f539990df746617110f22cb15abcb99
pashutetto <71449006+pashutetto@users.noreply.github.com>
2023-03-05 21:26:19 +0300

    Add files via upload

---
+++ taskspring3/usegauss.py
@@ -0,0 +1,14 @@
+import gauss
+from gauss import *
+
+a = array([
+    [1.5, 2.0, 1.5, 2.0],
+    [3.0, 2.0, 4.0, 1.0],
+    [1.0, 6.0, 0.0, 4  ],
+    [2.0, 1.0, 4.0, 3  ]
+], dtype=float)
+
+b = array([5, 6, 7, 8], dtype=float)
+
+print(solution(a,b))
+print(max_dev(a,b))
