7a59abefb48c2857a8e7623c7c42babdf7f64a16
pashutetto <71449006+pashutetto@users.noreply.github.com>
2023-05-10 22:19:01 +0300

    Add files via upload

---
+++ taskspring8/taskspring8.py
@@ -0,0 +1,69 @@
+import random
+import flask
+from flask import Flask, render_template, request, flash
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'dfhsdfkhgbh'
+
+
+answers = ["да", "нет", "может быть", "никогда", "возможно", "не знаю"]
+
+@app.context_processor
+def inject_globals():
+    return {
+        "songs":[
+            "Paranoid Android - Radiohead",
+            "When You Sleep - my bloody valentine",
+            "Machine Gun - Slowdive",
+            "When the Sun Hits - Slowdive",
+            "Million Year Summer - The Angelic Process",
+            "Acoustic - Ling tosite sigure",
+            "Breadcumb Train - Slint",
+            "Karma police - Radiohead"
+        ]
+    }
+@app.route("/")
+def index():
+    return flask.render_template(
+        'index.html'
+    )
+
+@app.route("/music")
+def music():
+    return flask.render_template(
+        'music.html'
+    )
+
+@app.route("/stories")
+def stories():
+    return flask.render_template(
+        'stories.html'
+    )
+
+@app.route("/askquestion", methods =['GET', 'POST'])
+def ansque():
+    if request.method == 'GET':
+        quest = request.args.get('question')
+
+    elif request.method == 'POST':
+        quest = request.form.get('question')
+        flash(random.choice(answers))
+
+    
+    if quest is None:
+        quest = "Введите вопрос, пожалуйста"
+    
+    return flask.render_template(
+        'askquestion.html',
+        question = quest,
+        method = request.method
+    )
+
+@app.route("/secret")
+def surprise():
+    return flask.render_template(
+        'secret.html'
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
